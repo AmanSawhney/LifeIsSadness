78f1c3cbe63c9793370b24000fa81f445324a7fb
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-20 18:41:06 -0400

    Added scraping logic to the welcome helper

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,17 @@
+require 'open-uri'  
+require 'nokogiri'
 module WelcomeHelper
+	def scrape()
+		links = []
+		source = open("https://www.reddit.com/r/HorriblyDepressing/",    "User-Agent" => "Code-Maven-Example (see: http://code-maven.com/download-a-page-using-ruby )")
+		page = Nokogiri::HTML(source)
+		page.css('a.title').each do |link|
+			if link['href'].include?('http')
+				links << { content: link.content, href: link['href'] }
+			else
+				links << { content: link.content, href: "http://reddit.com" + link['href'] }
+			end
+		end
+		return links[rand(links.size)][:href]
+	end
 end
